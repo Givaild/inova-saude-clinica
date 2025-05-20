c504d4034c4274a5a0e78eab3a98857e036e5fc7
Givaildo de Andrade <givaildo.oliveira@synvia.com>
2025-05-20 11:52:33 -0300

    fix: insert meta dados and ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,39 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Clínica Inova Saúde | Especialidades em Hortolândia',
+  description: 'A Clínica Inova Saúde oferece atendimento em Hortolândia. Agende sua consulta conosco!',
+  generator: 'Next.js',
+  keywords: ['Clínica Médica', 'Hortolândia', 'Psicologia', 'Consulta', 'Inova Saúde', 'Fonoaudiologia', 'Pilates'],
+  authors: [{ name: 'Clínica Inova Saúde', url: 'https://www.seusite.com.br' }],
+  applicationName: 'Clínica Inova Saúde',
+  referrer: 'origin-when-cross-origin',
+  colorScheme: 'light',
+  creator: 'Inova Saúde',
+  publisher: 'Inova Saúde',
+  openGraph: {
+    title: 'Clínica Inova Saúde | Especialidades em Hortolândia',
+    description: 'Conheça a Clínica Inova Saúde. Atendimento humanizado com qualidade e compromisso com sua saúde.',
+    url: 'https://www.seusite.com.br',
+    siteName: 'Clínica Inova Saúde',
+    images: [
+      {
+        url: '/fachada.jpg', // imagem usada em compartilhamentos
+        width: 1200,
+        height: 630,
+        alt: 'Clínica Inova Saúde Fachada',
+      },
+    ],
+    locale: 'pt_BR',
+    type: 'website',
+  },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+  },
 }
+
 
 export default function RootLayout({
   children,
@@ -13,7 +42,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body>{children}</body>
     </html>
   )
